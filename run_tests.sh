43e5180ca07919d58cf0f87851b229b46d6782e4
Michael Larson <mlarson@westernintech.com>
2018-03-19 14:27:36 -0700

    try running inside container

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,6 @@
+sudo modprobe can
+sudo modprobe can_raw
+sudo modprobe vcan
+sudo ip link add dev vcan0 type vcan || true
+sudo ip link set up vcan0 || true
 cansend vcan0 04D70301#1122334455667788
